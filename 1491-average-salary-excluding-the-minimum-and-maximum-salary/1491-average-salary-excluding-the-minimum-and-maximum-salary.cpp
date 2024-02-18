79e479989e65e0212472d90030206efa0d28b3e3
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-02-18 12:22:49 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (45.69%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -4,16 +4,11 @@ public:
         sort(salary.begin(), salary.end());
         vector<double>v;
         int div=salary.size()-2;
+        double sum=0;
         for(int i=1;i<salary.size()-1;i++)
         {
-            v.push_back((1.0*salary[i])/div);
+            sum+=((1.0*salary[i])/div);
         }
-        double sum=0;
-        for(int i=0;i<v.size();i++)
-        {
-            sum+=v[i];
-        }
-        
         return sum;
         
     }
