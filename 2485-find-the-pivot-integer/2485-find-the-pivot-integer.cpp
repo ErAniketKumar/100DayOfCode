0918499d1858a7b21348d3fb338b621e2d45acc2
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-03-13 06:19:06 +0530

    Time: 3 ms (51.39%), Space: 7.1 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -3,14 +3,13 @@ public:
     int pivotInteger(int n) {
         if(n==1) return 1;
         int allsum=(n*(n+1))/2;
-        int lsum=0,rightsum=0;
-        int ans=-1;
+        int lsum=0;
         for(int i=1;i<=n;i++)
         {
             lsum+=i;
             if(allsum-lsum==0) return i;
             allsum-=i;
         }
-        return ans;
+        return -1;
     }
 };
