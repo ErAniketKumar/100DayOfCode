0d2d52cd88d6d3a10c635bdef4d9375bca1e38a6
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-04-02 20:07:34 +0530

    Update 0994-rotting-oranges.cpp

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -57,7 +57,7 @@ public:
             }
 
         }
-
+        //if any fresh orange find then not possible so retuen -1;
         for(int i=0;i<rows;i++)
         {
             for(int j=0;j<cols;j++)
