cc01d94cfed25420b17649512cc3d8bc64c1c427
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-04-13 17:30:19 +0530

    Time: 250 ms (13.32%), Space: 124 MB (5.29%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,16 +1,43 @@
 
+class Node
+{
+public:
+    int sum, data, row;
+    Node()
+    {
+    }
+    Node(int sum, int data, int row)
+    {
+        this->sum = sum;
+        this->data = data;
+        this->row = row;
+    }
+};
+
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+    vector<Node> helper;
     map<int, vector<int>> m;
     for (int i = 0; i < nums.size(); i++)
     {
         for (int j = 0; j < nums[i].size(); j++)
         {
-            m[i+j].push_back(nums[i][j]);
+            Node nd(i + j, nums[i][j], i);
+            helper.push_back(nd);
         }
     }
+
+  
+
+    for (auto nd : helper)
+    {
+        m[nd.sum].push_back(nd.data);
+       
+    }
+
     vector<int> ans;
+
     for (auto x : m)
     {
         reverse(x.second.begin(), x.second.end());
